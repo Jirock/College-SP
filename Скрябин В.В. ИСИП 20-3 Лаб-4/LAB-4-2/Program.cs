bd0a8c7b278c7945b88c320a496b7159f472e01d
PushOk <70839406+Jirock@users.noreply.github.com>
2021-11-09 15:51:02 +0900

    LAB-4

---
+++ Скрябин В.В. ИСИП 20-3 Лаб-4/LAB-4-2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB_4_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            People a = new People();
+            People b = (People)a.Clone();
+
+            b.Age = 19;
+            b.Name = "Sanya";
+
+            a.Dispose();
+
+            Console.WriteLine("Имя: {0} Возраст: {1}", b.Name, b.Age);
+            Console.ReadLine();
+        }
+    }
+
+    class People : ICloneable, IDisposable
+    {
+        public string Name = "Viktor";
+        public byte Age = 22;
+        public Object Clone()
+        {
+            return this.MemberwiseClone();
+        }
+        
+        public void Dispose()
+        {
+            Console.Beep();
+            Console.WriteLine("Disposed");
+        }
+    }
+}
