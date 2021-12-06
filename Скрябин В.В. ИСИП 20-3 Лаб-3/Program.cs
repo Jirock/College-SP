380d4b532b6671145ce7c323646a4999818ec28a
PushOk <70839406+Jirock@users.noreply.github.com>
2021-12-06 19:34:09 +0900

    LAB-3

---
+++ Скрябин В.В. ИСИП 20-3 Лаб-3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SRS_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 10;
+            int y = 9;
+            int z = 20;
+            int v = 200;
+
+            min(x, y);
+
+            min(z + x + y, v);
+
+            Console.ReadLine();
+        }
+
+        public static void min(int a, int b)
+        {
+            Console.WriteLine(a > b ? $"{a} больше чем {b}" : $"{a} меньше чем {b}");
+        }
+    }
+}
